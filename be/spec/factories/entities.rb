99032b2804de3000f6f87688c4e2057725e9aec3
kbacha <chewbacha@gmail.com>
2018-10-26 15:00:13 -0700

    Fix entity slug

--- be/spec/factories/entities.rb
+++ be/spec/factories/entities.rb
@@ -6,7 +6,7 @@ FactoryBot.define do
   factory :entity, class: Types::Entity do
     before(:create) { raise 'No persistance, just use build' }
 
-    slug { Faker::App.name.underscore }
+    slug { Faker::App.name.gsub(/\s+/, '-').underscore }
     path { Faker::File.file_name }
     type { Types::EntityType.values.sample }
 
