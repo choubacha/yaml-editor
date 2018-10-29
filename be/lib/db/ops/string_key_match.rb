84514b54de4db91c2f6c77d1d3e31a6bee77f09d
kbacha <chewbacha@gmail.com>
2018-10-29 09:23:37 -0700

    Document and update search algo

--- be/lib/db/ops/string_key_match.rb
+++ be/lib/db/ops/string_key_match.rb
@@ -19,22 +19,43 @@ class Db
 
       private
 
-      def measure(string)
+      # When we measure, we want to provide points for runs of character matches
+      # and decrease the score the more distance between matched characters.
+      def measure(string) # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
         index = 0
         score = 0
+
         needle.each_char do |needle_char|
+          # Extra points are awarded based on the proximity to the previous character.
+          #
+          extra_points = 5
+
+          # We want to progress but not passed the end of the OG string.
           while index < string.size
             curr_char = string[index]
+
             if curr_char == needle_char
+              # When we find the needle character we increase the score and stop looking
               score += 1
               break
-            elsif score >= 1
-              score += 1
+            else
+              # We didn't match, so we lose an extra point
+              extra_points -= 1
+
+              # And move along the string.
+              index += 1
             end
-            index += 1
           end
+
+          # If we got here we can apply extra points if possible and
+          # return early if we've gone past the end of the string
+          score += extra_points if extra_points.positive?
           return nil if index >= string.size
         end
+
+        # Return the ratio of the index we searched to under the points accumulated
+        # The purpose of this is to award points with a slight preference for those
+        # that are found closer to the left side of the string.
         score.to_f / index.to_f
       end
     end
